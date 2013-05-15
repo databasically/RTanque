845f520128149112022931b12ad71d1599381b6a
David Zarlengo <dzarlengo@simplymeasured.com>
2013-05-15 03:36:28 -0700

    add record/replay support Conflicts:
    	bin/rtanque
    	lib/rtanque/match.rb

--- lib/rtanque/match.rb
+++ lib/rtanque/match.rb
@@ -1,6 +1,7 @@
 module RTanque
   class Match
     attr_reader :arena, :bots, :shells, :explosions, :ticks, :max_ticks, :teams
+    attr_accessor :recorder
 
     def initialize(arena, max_ticks = nil, teams = false)
       @arena = arena
@@ -60,6 +61,8 @@ module RTanque
     end
 
     def tick
+      recorder.stop if finished? && recorder
+
       self.shells.tick
       self.bots.tick
       self.explosions.tick
